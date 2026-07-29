aa36b4ad41a01cd23bd548b9a3c20d4836380fe8
MdHuzaifa018 <mdhuzaifakhan018@gmail.com>
2026-07-29 20:54:51 +0530

    HighConcurrencyConfig

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(cors({
 }));
 
 app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // API Routes
 app.use('/api/auth', require('./routes/auth'));
